12140f084c3a4466ab71d04e68d88f705d072343
MicheleArg <michele.arg@hotmail.it>
2021-11-10 10:43:39 +0100

    project

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -7,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
   logo = 'assets/img/logo.png';
+  isMobile : boolean = false;
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.isMobile=window.matchMedia("only screen and (max-device-width: 992px)").matches;
   }
 
+  getBrowser(){
+    return !this.isMobile;
+  }
+
+  getMobile(){
+    return this.isMobile;
+  }
 }
