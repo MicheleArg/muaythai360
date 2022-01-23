9ea63edf600c7003baf18cb7e9e984a5b21e4c5d
MicheleArg <michele.arg@hotmail.it>
2022-01-23 19:25:10 +0100

    1

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TeamComponent implements OnInit {
   team='assets/img/team.jpg';
 
-  constructor() { }
+  constructor() { } 
 
   ngOnInit(): void {
   }
